5528e1c867ef437c855b9ec0e27092f8b205faca
Supriti Sarkar <supritisarkar@outlook.com>
2021-03-10 02:01:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter , Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -8,14 +8,14 @@ import Skills from "./components/Skills";
 function App() {
   return (
     <div>
-      <Router>
+      <HashRouter>
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/About" component={About} />
           <Route path="/Skills" component={Skills} />
         </Switch>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
